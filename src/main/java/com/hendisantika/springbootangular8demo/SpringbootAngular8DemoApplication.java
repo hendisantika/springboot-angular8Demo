d8d1335eb7ef9ef8de5de9e03a8d2a8a62c10e7e
hendisantika <hendisantika@yahoo.co.id>
2019-08-01 05:49:31 +0700

    #6 - Add Init Data User

--- src/main/java/com/hendisantika/springbootangular8demo/SpringbootAngular8DemoApplication.java
+++ src/main/java/com/hendisantika/springbootangular8demo/SpringbootAngular8DemoApplication.java
@@ -1,13 +1,30 @@
 package com.hendisantika.springbootangular8demo;
 
+import com.hendisantika.springbootangular8demo.entity.User;
+import com.hendisantika.springbootangular8demo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringbootAngular8DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAngular8DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("Naruto", "Sasuke", "Sakura", "Kakashi", "Shino").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@konohagakure.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
     }
 
 }
