1085d70ad9366f46b625d90c136ac055437503bf
hendisantika <hendisantika@yahoo.co.id>
2019-08-01 06:49:49 +0700

    #22 - Modify angular8 app.module.ts file

--- angular8-client/src/app/app.module.ts
+++ angular8-client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {UserListComponent} from './user-list/user-list.component';
 import {UserFormComponent} from './user-form/user-form.component';
-import {FormsModule} from "@angular/forms";
+import {UserService} from './service/user.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,10 @@ import {FormsModule} from "@angular/forms";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
