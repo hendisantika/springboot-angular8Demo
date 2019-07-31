138ffd56af8fb5970c8e0e074f71f7c29151deff
hendisantika <hendisantika@yahoo.co.id>
2019-08-01 05:44:13 +0700

    #1 - Initial Project setup

---
+++ src/main/java/com/hendisantika/springbootangular8demo/SpringbootAngular8DemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootangular8demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngular8DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAngular8DemoApplication.class, args);
+    }
+
+}
