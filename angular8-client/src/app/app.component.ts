3f40951abcc5114fe356b16c8d983968d649a247
hendisantika <hendisantika@yahoo.co.id>
2019-08-01 07:52:14 +0700

    #23 - Modify angular8 user.service.ts file

--- angular8-client/src/app/app.component.ts
+++ angular8-client/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = string;
+  private title: string;
 
   constructor() {
     this.title = 'Spring Boot - Angular Application';
